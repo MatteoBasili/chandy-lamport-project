63f5cbb5aeefbf6398cceb1351e064589b7689ab
MatteoBasili <matthew2000.mb@gmail.com>
2024-05-19 15:59:39 +0200

    Commit

---
+++ src/utils/state_test.go
@@ -0,0 +1,94 @@
+package utils_test
+
+import (
+	"sdccProject/src/utils"
+	"testing"
+)
+
+func TestNodeState_String(t *testing.T) {
+	nodeState := utils.NodeState{
+		NodeName: "Node1",
+		Balance:  100,
+		SentMsgs: map[string]utils.AppMessage{
+			"Node2": {Msg: utils.Message{ID: "msg1"}, IsMarker: false, From: 1, To: 2},
+			"Node3": {Msg: utils.Message{ID: "msg2"}, IsMarker: false, From: 1, To: 3},
+		},
+		ReceivedMsgs: []utils.AppMessage{
+			{Msg: utils.Message{ID: "msg3"}, IsMarker: false, From: 2, To: 1},
+			{Msg: utils.Message{ID: "msg4"}, IsMarker: false, From: 3, To: 1},
+		},
+		Busy: true,
+	}
+
+	expected := "- Balance: $100\n- Messages sent: [ msg1 -> Node2, msg2 -> Node3 ]\n- Messages received: [ msg3, msg4 ]"
+	if nodeState.String() != expected {
+		t.Errorf("Expected %q, but got %q", expected, nodeState.String())
+	}
+}
+
+func TestChState_String(t *testing.T) {
+	chState := utils.ChState{
+		RecvMsgs: []utils.AppMessage{
+			{Msg: utils.Message{ID: "msg1"}, IsMarker: false, From: 1, To: 2},
+			{Msg: utils.Message{ID: "msg2"}, IsMarker: false, From: 2, To: 1},
+		},
+		Recording: true,
+	}
+
+	expected := "(Channel is still recording!!!!!!!!!!!!) "
+	expected += "msg1, msg2"
+	if chState.String() != expected {
+		t.Errorf("Expected %q, but got %q", expected, chState.String())
+	}
+}
+
+func TestFullState_String(t *testing.T) {
+	fullState := utils.FullState{
+		Node: utils.NodeState{
+			NodeName: "Node1",
+			Balance:  100,
+			SentMsgs: map[string]utils.AppMessage{
+				"Node2": {Msg: utils.Message{ID: "msg1"}, IsMarker: false, From: 1, To: 2},
+			},
+			ReceivedMsgs: []utils.AppMessage{
+				{Msg: utils.Message{ID: "msg2"}, IsMarker: false, From: 2, To: 1},
+			},
+			Busy: true,
+		},
+		Channels: map[int]utils.ChState{
+			1: {RecvMsgs: []utils.AppMessage{{Msg: utils.Message{ID: "msg3"}, IsMarker: false, From: 3, To: 1}}},
+			2: {RecvMsgs: []utils.AppMessage{{Msg: utils.Message{ID: "msg4"}, IsMarker: false, From: 1, To: 2}}},
+		},
+		AllMarksRecv: false,
+	}
+
+	expected := "\nState:\n- Balance: $100\n- Messages sent: [ msg1 -> Node2 ]\n- Messages received: [ msg2 ]\nChannels:\n[ 1 ] ==> msg3\n[ 2 ] ==> msg4\n----------------NOT RECEIVED ALL MARKS---------"
+	if fullState.String() != expected {
+		t.Errorf("Expected %q, but got %q", expected, fullState.String())
+	}
+}
+
+func TestGlobalState_String(t *testing.T) {
+	globalState := utils.GlobalState{
+		GS: []utils.FullState{
+			{
+				Node: utils.NodeState{
+					NodeName: "Node1",
+					Balance:  100,
+				},
+				Channels: map[int]utils.ChState{
+					1: {RecvMsgs: []utils.AppMessage{{Msg: utils.Message{ID: "msg1"}, IsMarker: false, From: 2, To: 1}}},
+				},
+				AllMarksRecv: true,
+			},
+		},
+	}
+
+	expected := "\n--------------------------------------Node Node1: \nState:\n- Balance: $100\n- Messages sent: [ ]\n- Messages received: [ ]\nChannels:\n[ 1 ] ==> msg1\n"
+	if globalState.String() != expected {
+		t.Errorf("Expected %q, but got %q", expected, globalState.String())
+	}
+}
+
+/*"- Balance: $100\n- Messages sent: [ msg1 -> Node2, msg2 -> Node3 ]\n- Messages received: [ msg3, msg4 ]"
+"- Balance: $100\n- Messages sent: [ msg1 -> Node2, msg2 -> Node3 ]\n- Messages received: [ msg3, msg4 ]"*/
